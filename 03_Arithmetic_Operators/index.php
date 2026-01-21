2fc9fb59d583f907179dc0e05bd2e56f74280f8e
bezzinag <bezzinag@yahoo.com>
2026-01-21 19:45:58 +0100

    Arithmetic Operators

---
+++ 03_Arithmetic_Operators/index.php
@@ -0,0 +1,24 @@
+<?php
+    $x = null;
+    $y = 6;
+    $z = 10;
+
+    //$x = $y + $z;
+    //$x = $y - $z;
+    //$x = $y * $z;
+    //$x = $y / $z;
+    $x = $y ** $z; //power of
+    //modulus %
+
+    
+    //echo $x;
+    
+
+    $counter = 0;
+    $counter++;
+
+    echo $counter;
+
+    //operator precedence - > BODMAS
+
+?>
