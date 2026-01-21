8ef1aa8cb503e2e428e21da3979459dd20e32bb3
bezzinag <bezzinag@yahoo.com>
2026-01-21 16:30:30 +0100

    Introduction to php

---
+++ 01_intro/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "i like coding <br>";
+    echo "it is really nice";
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>test</title>
+
+    </head>
+
+    <body>
+        <br>
+        <button>test</button>
+    </body>
+
+</html>
